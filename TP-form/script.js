97ceb4d29c8e70010b803d2092ed4efc5b4ab133
Joulos-dev <jules.fournol@hotmail.com>
2025-06-02 14:18:13 +0200

    TP-form fini

--- TP-form/script.js
+++ TP-form/script.js
@@ -1,22 +1,55 @@
 let form = document.querySelector("form");
-console.log(form);
-const prenom = document.querySelector("#prenom");
-console.log(prenom);
-const nom = document.querySelector("#nom");
-console.log(nom);
-const age = document.querySelector("#age");
-console.log(age);
-const sexe = form.sexe;
-console.log(sexe);
-const profession = document.querySelector("#profession");
-console.log(profession);
-const region = document.querySelector("#region");
-console.log(region);
-const hobbies = form.hobbies;
-console.log(hobbies);
-const lien = document.querySelector("#lien-linked-in");
-console.log(lien);
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+const prenom = document.querySelector("#prenom");
+const nom = document.querySelector("#nom");
+const age = document.querySelector("#age");
+const sexe = form.sexe;
+const profession = document.querySelector("#profession");
+const region = document.querySelector("#region");
+const hobbies = form.hobbies;
+const lien = document.querySelector("#lien-linked-in");
+
+const cardPrenom = document.querySelector(".cardPrenom");
+console.log(cardPrenom);
+const cardNom = document.querySelector(".cardNom");
+console.log(cardNom);
+const cardAge = document.querySelector(".cardAge");
+console.log(cardAge);
+const cardSexe = document.querySelector(".cardSexe");
+console.log(cardSexe);
+const cardProfession = document.querySelector(".cardProfession");
+console.log(cardProfession);
+const cardRegion = document.querySelector(".cardRegion");
+console.log(cardRegion);
+const cardHobbies = document.querySelector(".cardHobbies");
+console.log(cardHobbies);
+const cardLien = document.querySelector(".cardLien");
+console.log(cardLien);
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    cardPrenom.innerHTML = prenom.value;
+    cardNom.innerHTML = nom.value;
+    cardAge.innerHTML = age.value;
+
+    for (let i = 0; i < sexe.length; i++) {
+        if (sexe[i].checked === true) {
+            cardSexe.innerHTML = sexe[i].value;
+        }
+    }
+
+    cardProfession.innerHTML = profession.value;
+    cardRegion.innerHTML = region.value;
+    cardHobbies.innerHTML = "";
+
+    for (let i = 0; i < hobbies.length; i++) {
+        if (hobbies[i].checked === true) {
+            cardHobbies.innerHTML += " / " + hobbies[i].value;
+        }
+    }
+
+    cardLien.innerHTML = lien.value;
+
+    console.log(event);
 });
